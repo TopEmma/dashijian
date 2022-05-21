17d2d803626a70a93389b81eca0e4e92b4e7f1de
TopEmma <1581917161@qq.com>
2022-05-21 10:22:59 +0800

    完成了首页

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -7,7 +7,7 @@ $(function() {
             }
             option.url = `http://www.liulongbin.top:3007` + option.url
         })
-        // 统一处理权限问题
+        // 统一处理了权限问题
     option.complete = (res) => {
         // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
         if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
